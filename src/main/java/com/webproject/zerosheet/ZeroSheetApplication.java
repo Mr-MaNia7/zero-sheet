2bb2bd760002329558536c08191cc49fb7fca1d5
Yonathan <yoni.tesse@gmail.com>
2022-01-18 14:53:34 +0300

    adding spring files

---
+++ src/main/java/com/webproject/zerosheet/ZeroSheetApplication.java
@@ -0,0 +1,13 @@
+package com.webproject.zerosheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroSheetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeroSheetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webproject/zerosheet/ZeroSheetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webproject.zerosheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroSheetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
