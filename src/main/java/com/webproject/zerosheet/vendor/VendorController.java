30296a0586f24380f0c45aab81cd95bd9cec9845
Yonathan <58274345+yonit1dev@users.noreply.github.com>
2022-01-20 11:50:41 +0300

    editing classes and entities

--- src/main/java/com/webproject/zerosheet/vendor/VendorController.java
+++ src/main/java/com/webproject/zerosheet/vendor/VendorController.java
@@ -1,4 +1,26 @@
 package com.webproject.zerosheet.vendor;
 
+import com.webproject.zerosheet.customer.CustomerController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/vendors")
 public class VendorController {
+    
+    private final VendorService vendorService;
+
+    @Autowired
+    public VendorController(VendorService vendorService){
+        this.vendorService = vendorService;
+    }
+
+    @GetMapping
+    public List<Vendor> getVend(){
+        return vendorService.getVend();
+    }
 }
--- src/main/java/com/webproject/zerosheet/vendor/VendorService.java
+++ src/main/java/com/webproject/zerosheet/vendor/VendorService.java
@@ -1,4 +1,12 @@
 package com.webproject.zerosheet.vendor;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class VendorService {
+    public List<Vendor> getVend(){
+        return List.of(new Vendor());
+    }
 }
