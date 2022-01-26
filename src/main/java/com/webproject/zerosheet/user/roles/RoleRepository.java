bb549243756f1e75b30494535214ba0cc10e823b
Bethi <96546826+bethi2@users.noreply.github.com>
2022-01-26 16:49:43 +0300

    Delete src/main/java/com/webproject/zerosheet/user/roles directory

--- src/main/java/com/webproject/zerosheet/user/roles/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.webproject.zerosheet.user.roles;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-}
